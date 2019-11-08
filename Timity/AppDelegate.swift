f599b5b00d6c4dff61ed85e66967742488f9f545
Bassel Al Araaj <info@basselalaraaj.com>
2019-11-08 17:58:24 +0100

    first commit

---
+++ Timity/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  Timity
+//
+//  Created by Bassel Al Araaj on 08/11/2019.
+//  Copyright © 2019 Webicom B.V. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        statusItem.button?.title = "Timity"
+        statusItem.button?.target = self
+        statusItem.button?.action = #selector(showSettings)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+    }
+    
+    @objc func showSettings(){
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        guard let vc = storyboard.instantiateController(withIdentifier: "ViewController") as? ViewController else { fatalError("Unable to find ViewController in the storyboard.")}
+        
+        guard let button = statusItem.button else {
+            fatalError("Couldn't find status item button.")
+        }
+        
+        let popoverView = NSPopover()
+        popoverView.contentViewController = vc
+        popoverView.behavior = .transient
+        popoverView.show(relativeTo: button.bounds, of: button, preferredEdge: .maxY)
+    }
+
+}
+
---
+++ Timity/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Timity
+//
+//  Created by Bassel Al Araaj on 08/11/2019.
+//  Copyright © 2019 Webicom B.V. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
