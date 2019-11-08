2af18c4f080de81f8d818761ad7cb39db9d44597
Bassel Al Araaj <info@basselalaraaj.com>
2019-11-08 18:33:04 +0100

    adding stop and start button with timer

--- Timity/ViewController.swift
+++ Timity/ViewController.swift
@@ -9,11 +9,33 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var timerLabel: NSTextFieldCell!
+    
+    var duration = 0
+    var timer = Timer()
+    var isTimerOn = false
+    
+    @IBAction func startTimer(_ sender: Any) {
+        if(self.isTimerOn == false) {
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {[weak self] (_) in
+                guard let strongSelf = self else {return}
+                strongSelf.duration += 1
+                strongSelf.timerLabel.title = String(strongSelf.duration)
+            })
+            self.isTimerOn = true
+        }
+    }
+    
+    @IBAction func stopTimer(_ sender: Any) {
+        self.timer.invalidate()
+        self.duration = 0
+        self.isTimerOn = false
+        self.timerLabel.title = String(self.duration)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.timerLabel.title = String(self.duration)
     }
 
     override var representedObject: Any? {
