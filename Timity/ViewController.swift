80b9953d22028509de394897c649c3f0df01f11d
Bassel Al Araaj <info@basselalaraaj.com>
2019-11-10 00:27:43 +0100

    adding tasks and buttons

--- Timity/ViewController.swift
+++ Timity/ViewController.swift
@@ -8,51 +8,45 @@
 
 import Cocoa
 
+class TaskTableViewCell: NSTableCellView {
+    @IBOutlet weak var taskTitle: NSTextField!
+    @IBOutlet weak var startPauseButton: NSButton!
+    @IBAction func stopTimer(_ sender: Any) {
+        timerModel?.stopTimer()
+    }
+    @IBAction func toggleTimer(_ sender: Any) {
+        if(timerModel?.isTimerOn == false) {
+            timerModel?.startTimer()
+            startPauseButton.image = NSImage(named: "NSTouchBarPauseTemplate")
+        } else {
+            timerModel?.pauseTimer()
+            startPauseButton.image = NSImage(named: "NSTouchBarPlayTemplate")
+        }
+    }
+}
+
 class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
     @IBOutlet weak var taskTable: NSTableView!
     
     let list = ["Taak 1", "Taak 2", "Taak 3", "Taak 4", "Taak 5"]
-//    var startName: String = "Start"
-//    var pauseName: String = "Pause"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         timerModel?.continueTimer()
-//        if(timerModel?.isTimerOn == true) {
-//            self.startPauseButton?.title = pauseName
-//        } else {
-//            self.startPauseButton?.title = startName
-//        }
     }
     
     func numberOfRows(in tableView: NSTableView) -> Int {
         return list.count
     }
     
-    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
-        debugPrint(tableColumn!.identifier.rawValue)
-        if tableColumn!.identifier.rawValue == "task"{
-            return list[row]
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "TaskCell"), owner: self) as? TaskTableViewCell else { return nil }
+        
+        if (tableColumn?.identifier)!.rawValue == "task" {
+            cell.taskTitle.stringValue = list[row]
         }
-        if tableColumn!.identifier.rawValue == "action"{
-            return "Start"
-        }
-        return ""
-    }
-    
-    
-    @objc func startTimer() {
-        if(timerModel?.isTimerOn == false) {
-            timerModel?.startTimer()
-//            self.startPauseButton?.title = pauseName
-        } else {
-            timerModel?.pauseTimer()
-//            self.startPauseButton?.title = startName
-        }
-    }
-
-    @objc func stopTimer() {
-        timerModel?.stopTimer()
+        
+        return cell
     }
 
     override var representedObject: Any? {
